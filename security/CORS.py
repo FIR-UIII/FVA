8a870500908987fb1efa8a83631f426c3c6b5887
FIR-UIII <art.paderin@gmail.com>
2024-07-17 18:15:03 +0300

    remove CSP, CORS from MAIN

---
+++ security/CORS.py
@@ -0,0 +1,11 @@
+from flask_cors import CORS
+
+def setup_cors(app):
+    cors = CORS(app, resources={
+        r"/*": {
+            "origins": "*",  # allow any origin to request resources from site
+            "methods": ["GET", "POST"],
+            "headers": ["Content-Type", "Authorization"],
+            "credentials": False
+        }
+    })
